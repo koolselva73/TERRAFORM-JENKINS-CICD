bfe9f965a86caed8de995adce90e801a4d6e81cd
koolselva73 <71598783+koolselva73@users.noreply.github.com>
2025-01-29 15:38:04 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "myjenkins-29jan2025"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
+    region         = "ap-southeast-2"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
   }
 }
